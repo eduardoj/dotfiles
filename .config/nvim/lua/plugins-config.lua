d299096c46578713dcd022ee6f06ecd9342e7015
Eduardo Navarro <enavarro@suse.com>
2021-11-10 21:47:12 +0100

    Add perl lsp

--- .config/nvim/lua/plugins-config.lua
+++ .config/nvim/lua/plugins-config.lua
@@ -121,9 +121,12 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+-- perlpls requires the `pls` executable: `cpan> install PLS`
+nvim_lsp.perlpls.setup{}
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'solargraph' }
+local servers = { 'perlpls', 'solargraph' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
