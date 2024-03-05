536f22e338f84995d0d61d359c31dc351e2ee1a1
Eduardo Navarro <enavarro@suse.com>
2024-03-05 01:15:54 +0100

    Change colorscheme from tokyo to gruvbox

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -732,12 +732,17 @@ require('lazy').setup {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    opts = {
+      -- contrast = 'hard',
+      transparent_mode = true,
+    },
+    config = function(_, opts)
+      require('gruvbox').setup(opts)
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
