95c0e865427bf9703055ea9e57e9a48b449fb7ed
Eduardo Navarro <enavarro@suse.com>
2025-05-23 09:17:49 +0200

    Update with changes from upstream

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -492,8 +492,8 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = {}, version = 'v1.*' },
-      { 'mason-org/mason-lspconfig.nvim',  version = 'v1.*' },
+      { 'mason-org/mason.nvim', opts = {} },
+      'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
@@ -665,6 +665,12 @@ require('lazy').setup({
         },
       }
 
+      -- LSP servers and clients are able to communicate to each other what features they support.
+      --  By default, Neovim doesn't support everything that is in the LSP specification.
+      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -747,9 +753,12 @@ require('lazy').setup({
         automatic_installation = false,
         handlers = {
           function(server_name)
-            local config = servers[server_name] or {}
-            vim.lsp.config(server_name, config)
-            vim.lsp.enable(server_name)
+            local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
           end,
         },
       }
