af5fad5b508b76911d59dd2c2aa9f47e115d4d98
Eduardo Navarro <enavarro@suse.com>
2025-12-03 19:25:58 +0100

    Enable gemini in avante

--- .config/nvim/lua/custom/plugins/avante.lua
+++ .config/nvim/lua/custom/plugins/avante.lua
@@ -35,6 +35,21 @@ return {
     --     },
     --   },
     -- },
+    provider = "gemini",
+    providers = {
+      gemini = {
+        endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+        model = "gemini-2.5-flash",
+        timeout = 30000, -- Timeout in milliseconds
+        context_window = 1048576,
+        use_ReAct_prompt = true,
+        extra_request_body = {
+          generationConfig = {
+            temperature = 0.75,
+          },
+        },
+      },
+    },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
