0a9bacf4f0147c0dd904d6641d7946b4542d6060
Eduardo Navarro <enavarro@suse.com>
2026-05-18 17:14:29 +0200

    Use kickstart.plugins.lint (nvim-lint)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1718,7 +1718,7 @@ do
   --
   require 'kickstart.plugins.debug'
   require 'kickstart.plugins.indent_line'
-  -- require 'kickstart.plugins.lint'
+  require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
   require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
